59a6e769f4517bc00907959223b067fcb5a5205e
Hayes Barber <barberhayes@gmail.com>
2025-08-08 20:45:02 -0400

    library file

--- lib/flowkit.dart
+++ lib/flowkit.dart
@@ -1,1 +1,9 @@
+library;
 
+export 'src/flow_definition.dart';
+export 'src/flow_registry.dart';
+export 'src/flow_starter.dart';
+export 'src/navigation_extensions.dart';
+export 'src/navigation_service.dart';
+export 'src/nested_navigator_provider.dart';
+export 'src/slide_bottom_route.dart';
