7421d6569e3169734a58e5052dcd1425d6657080
Hayes Barber <barberhayes@gmail.com>
2025-08-08 20:28:44 -0400

    auto commit

---
+++ lib/src/navigation_extensions.dart
@@ -0,0 +1,47 @@
+import 'package:flowkit/src/navigation_service.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+extension NavKeyExtension on GlobalKey<NavigatorState> {
+  void popUntilFirst() {
+    return currentState?.popUntil((route) => route.isFirst);
+  }
+
+  Future<dynamic>? push(Widget page, {bool replace = false}) {
+    return currentState?.push(MaterialPageRoute(builder: (_) => page));
+  }
+
+  Future<dynamic>? pushWithProvider<T extends ChangeNotifier>(
+    Widget page,
+    T provider,
+  ) {
+    return currentState?.push(
+      MaterialPageRoute(
+        builder: (_) => MultiProvider(
+          providers: [ChangeNotifierProvider.value(value: provider)],
+          child: page,
+        ),
+      ),
+    );
+  }
+}
+
+extension Nav on NavigatorState {
+  Future<dynamic>? pushWidget(Widget page) {
+    return push(MaterialPageRoute(builder: (_) => page));
+  }
+
+  Future<dynamic>? pushWithProvider<T extends ChangeNotifier>(
+    Widget page,
+    T provider, {
+    bool replace = false,
+  }) {
+    return Navigation.I.push(
+      MultiProvider(
+        providers: [ChangeNotifierProvider.value(value: provider)],
+        child: page,
+      ),
+      replace: replace,
+    );
+  }
+}
